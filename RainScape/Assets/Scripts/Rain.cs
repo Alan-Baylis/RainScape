9d644977cf282c5fc6e9dab504b4797355a65b3f
whiteSkar <sypower07@hotmail.com>
2016-03-04 18:24:29 -0800

    rain kills player. only on origin right now

---
+++ RainScape/Assets/Scripts/Rain.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rain : MonoBehaviour
+{
+    public Transform player;
+    public float rainInterval;
+    
+    private float prevRainTime;
+    void Start()
+    {
+        prevRainTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - prevRainTime >= rainInterval)
+        {
+            var hit = Physics2D.Raycast(Camera.main.transform.position, -Vector2.up);
+            if (hit.collider)
+            {
+                var hitGameObject = hit.collider.gameObject;
+                if (hitGameObject.CompareTag("Player"))
+                {
+                    Destroy(hitGameObject);
+                }
+            }
+            
+            prevRainTime = Time.time;
+        }
+    }
+}
