d6f091194d37276d35ee9b7babb66e3f74fb5f14
whiteSkar <sypower07@hotmail.com>
2016-03-04 17:26:36 -0800

    player movement with boundary

--- RainScape/Assets/Scripts/PlayerMovement.cs
+++ RainScape/Assets/Scripts/PlayerMovement.cs
@@ -18,6 +18,31 @@ public class PlayerMovement : MonoBehaviour
         var moveVert = Input.GetAxisRaw("Vertical") * Time.deltaTime * speed;
         
         var curPos = transform.position;
-        transform.position = new Vector3(curPos.x + moveHori, curPos.y + moveVert, 0);
+        var dest = new Vector3(curPos.x + moveHori, curPos.y + moveVert, 0);
+        Move(dest);
+    }
+    
+    private void Move(Vector3 dest)
+    {
+        var viewportHalfWidth = Camera.main.orthographicSize * Screen.width / Screen.height;
+        var viewportHalfHeight = Camera.main.orthographicSize;
+        
+        var renderer = GetComponent<SpriteRenderer>();
+        var playerHalfWidth = renderer.bounds.size.x * 0.5f;
+        var playerHalfHeight = renderer.bounds.size.y * 0.5f;
+
+        if (dest.x < -viewportHalfWidth + playerHalfWidth)
+            dest.x = -viewportHalfWidth + playerHalfWidth;
+            
+        if (dest.x > viewportHalfWidth - playerHalfWidth)
+            dest.x = viewportHalfWidth - playerHalfWidth;
+            
+        if (dest.y < -viewportHalfHeight + playerHalfHeight)
+            dest.y = -viewportHalfHeight + playerHalfHeight;
+        
+        if (dest.y > viewportHalfHeight - playerHalfHeight)
+            dest.y = viewportHalfHeight - playerHalfHeight;
+            
+        transform.position = dest;
     }
 }
