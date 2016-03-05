b395895c5cbc3db61ecf02fcafea5d77321a033d
whiteSkar <sypower07@hotmail.com>
2016-03-04 16:53:21 -0800

    enable vscode integration

---
+++ RainScape/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        var moveSpeed = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
+        transform.Rotate(0, speed, 0);
+	}
+}
