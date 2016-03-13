20c739f69312517629b67a426a1c3aed050edbd0
whiteSkar <sypower07@hotmail.com>
2016-03-12 22:32:35 -0800

    more rain

--- RainScape/Assets/Scripts/RainManager.cs
+++ RainScape/Assets/Scripts/RainManager.cs
@@ -4,42 +4,33 @@ using System.Collections;
 public class RainManager : MonoBehaviour
 {
     public GameObject rain;
-    public int rainRowCount;
-    public int rainColCount;
     public float rainInterval;
+    public int numRainAtOnce;
     
-    private float rowInterval;
-    private float colInterval;
     
     void Start()
     {
-        // alot of redundant usage. refactor
-        var viewportHalfWidth = Camera.main.orthographicSize * Screen.width / Screen.height;
-        var viewportHalfHeight = Camera.main.orthographicSize;
-        
-        colInterval = viewportHalfWidth * 2.0f / (rainColCount + 1);
-        rowInterval = viewportHalfHeight * 2.0f / (rainRowCount + 1);
-        
         StartCoroutine(PutRain());
-        
     }
 
     IEnumerator PutRain()
-    {
+    {            
+        var maxY = Camera.main.orthographicSize;
+        var minY = -maxY;
+        var maxX = Camera.main.orthographicSize * Screen.width / Screen.height;
+        var minX = -maxX;
+        
         while (true)
         {
             yield return new WaitForSeconds(rainInterval);
-            //yield return null;
             
-            var maxY = rowInterval * rainRowCount / 2.0f;
-            var minY = -maxY;
-            var maxX = colInterval * rainColCount / 2.0f;
-            var minX = -maxX;
-            
-            var posY = Random.Range(minY, maxY);
-            var posX = Random.Range(minX, maxX);
-            
-            Instantiate(rain, new Vector3(posX, posY, 0), Quaternion.identity);
+            for (int i = 0; i < numRainAtOnce; i++)
+            {
+                var posY = Random.Range(minY, maxY);
+                var posX = Random.Range(minX, maxX);
+                
+                Instantiate(rain, new Vector3(posX, posY, 0), Quaternion.identity);
+            }
         }
     }
 }
